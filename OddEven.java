04e113c09e3f23b43e6bae9faa7afd7256c5f6d7
vanshika898 <vanshikanigam8989@gmail.com>
2024-12-21 23:02:17 +0530

    basic

---
+++ OddEven.java
@@ -0,0 +1,10 @@
+public class OddEven {
+    public static void main(String[] args) {
+        int i = 100;
+        if (i % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
---
+++ ternary.java
@@ -0,0 +1,7 @@
+public class ternary {
+    public static void main(String[] gg) {
+        int i = 90;
+        String s = (i % 2 != 0) ? "odd" : "even";
+        System.out.println(s);
+    }
+}
